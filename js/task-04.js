906f0af748f0ac84f2676c6d627897b74e76209d
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-04 00:05:45 +0000

    task_4-5

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,18 @@
-const counter = document.querySelector("#counter");
 const span = document.querySelector("#value");
 const btnDecr = document.querySelector('[data-action = "decrement"]');
 const btnIncr = document.querySelector('[data-action = "increment"]');
-console.log(counter);
+
 console.log(span);
 console.log(btnDecr);
 console.log(btnIncr);
 let counterValue = 0;
 
-btnDecr.addEventListener("click", () => {
+btnIncr.addEventListener("click", () => {
     counterValue += 1;
     span.textContent = counterValue;
 });
-btnIncr.addEventListener("click", () => {
+
+btnDecr.addEventListener("click", () => {
     counterValue -= 1;
     span.textContent = counterValue;
 });
