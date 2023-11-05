896012287dacdb5f47a2e612b38e16e1457b0355
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-05 19:01:05 +0000

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+console.log(changeColorBtn);
+console.log(colorSpan);
+
+changeColorBtn.addEventListener("click", function () {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+});
