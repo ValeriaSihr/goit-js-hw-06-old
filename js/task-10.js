1d7ec3757a91e946eaadd33f59ffda6e2c9afb2b
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-05 21:37:21 +0000

    t10_I_dont_know_how_to_do
    I don`t understand how to finish this task, please help

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+const controls = document.querySelector("#controls");
+console.log(controls);
+const boxes = document.querySelector("#boxes");
+console.log(boxes);
+function createBoxes(amount) {}
