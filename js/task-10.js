5915a348fbc9b938f30e8ccea5e67c4c75f6bb81
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-05 22:40:14 +0000

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,8 +3,33 @@ function getRandomHexColor() {
         .toString(16)
         .padStart(6, 0)}`;
 }
-const controls = document.querySelector("#controls");
-console.log(controls);
-const boxes = document.querySelector("#boxes");
-console.log(boxes);
-function createBoxes(amount) {}
+
+const reference = {
+    controls: document.querySelector("#controls input"),
+    create: document.querySelector("button[data-create]"),
+    destroy: document.querySelector("button[data-destroy]"),
+    boxes: document.querySelector("#boxes"),
+};
+console.log(reference);
+
+function createBoxes(amount) {
+    for (let i = 0; i < amount; i++) {
+        const elem = document.createElement("div");
+
+        elem.style.width = 30 + 10 * i + "px";
+        elem.style.height = 30 + "px";
+        elem.style.backgroundColor = getRandomHexColor();
+
+        reference.boxes.append(elem);
+    }
+}
+reference.create.addEventListener("click", function () {
+    const amount = reference.controls.value;
+    if (amount > 0) {
+        createBoxes(amount);
+    }
+});
+
+reference.destroy.addEventListener("click", function () {
+    reference.boxes.innerHTML = "";
+});
